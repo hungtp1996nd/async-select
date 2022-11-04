5db6da44cec9ff7dc1b345f866b0df92dbe72819
Truong Phuc Hung <hungtp1996nd@gmail.com>
2022-11-04 15:09:24 +0700

    async select

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import AsyncSelect from 'react-select/async'
+import request from './apis/request';
 
 function App() {
+  const [items, setItems] = useState<any>([])
+  const [keyProjects, setKeyProjects] = useState<string>('')
+  const loadOptions = (value: string, callback: (options: any) => void) => {
+    request.get('/items').then(data => {
+      const newData = (data.data || [])?.map((item: any) => ({
+        value: item.name, 
+        label: item.name,
+        id: item.id
+      }))
+      callback(newData)
+    })
+  }
+  const loadProjects = (value: string, callback: (options: any) => void) => {
+    console.log(items, keyProjects)
+    request.get(`/projects?itemId=${items[0]?.id}`).then(data => {
+      const newData = (data.data || [])?.map((item: any) => ({
+        value: item.name, 
+        label: item.name
+      }))
+      callback(newData)
+    })
+  }
+  useEffect(() => {
+    let keys = ''; 
+    (items || [])?.forEach((item: any) => {
+      keys = keys + item.name
+    })
+    setKeyProjects(keys)
+  }, [items])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AsyncSelect 
+        isMulti
+        value={items}
+        onChange={setItems}
+        cacheOptions={true}
+        loadOptions={loadOptions}
+        defaultOptions={true} 
+      />
+      <AsyncSelect 
+        isMulti
+        key={keyProjects}
+        cacheOptions={true}
+        loadOptions={loadProjects}
+        defaultOptions={true} 
+      />
     </div>
   );
 }
